32da2772c8d1393f5a9ef3d1ac2d1a4f18854a40
Eirini Milaiou <eirinimilaiou@eirinis-mbp.home>
2016-06-01 19:34:02 +0100

    adding stemming and stop words

--- PreProcess.py
+++ PreProcess.py
@@ -8,13 +8,16 @@ import re
 import glob, os
 import numpy as np
 from nltk.tokenize import RegexpTokenizer
-#from stop_words import get_stop_words
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import matplotlib.pyplot as plt
 from gensim import corpora, models
 #en_stop = get_stop_words('en')
 from sklearn import preprocessing
 import unicodedata
+operators = set(('Ive'))
+cachedStopWords = set(stopwords.words("english")) + operators
+
 
 tokenizer = RegexpTokenizer(r'\w+')
 p_stemmer = PorterStemmer()
@@ -42,20 +45,22 @@ class PreProcess:
                         "choice_shops","choice_catering"
                         , "shopping_spend", "catering_spend"]
         data = data[data.comment != "None" ]
+        data = data[data.comment != "See Raw Data" ]
+        data = data[data.comment != "See raw data" ]
         data.replace(to_replace=-1, value=np.nan, inplace=True, regex=True)
         #pd.to_numeric(data.recommendation,errors='coerce')
         data = data.convert_objects(convert_numeric=True)
         #data.comment.toString()
         print(type(data.comment.ix[1]))
         return data
-    '''
+
     def removeSingleChars(subject):
         result = []
         for i in subject:
             if(len(i)>2):
                 result.append(i)
         return result
-    '''
+
     def removeNonLatin(subject):
         result = re.sub(r"[^\x00-\x7F]+","", subject)
         return result
@@ -63,7 +68,12 @@ class PreProcess:
     def initialProcess(self):
         data = self.loadToDataFrame()
         for i in range(0,len(data)):
-            raw_doc = self.removeNonLatin(data.comment.ix[i])
+            raw_doc = self.removeNonLatin(data.comment.ix[i].lower())
+            tokens = tokenizer.tokenize(raw_doc)
+            stopped_tokens = [i for i in tokens if not i in cachedStopWords]
+            stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+            self.corpus.append(self.removeSingleChars(stemmed_tokens))
+
 
 PreProcess("/Users/eirinimilaiou/Documents/ucl/CACI/test_data/","Example_Data.xlsx").loadToDataFrame()
 
